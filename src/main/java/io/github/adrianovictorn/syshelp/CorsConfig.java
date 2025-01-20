dbeb37dc842fe5961e002a8fb6c9c5db1c21ae85
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2025-01-19 21:48:20 -0300

    Update CorsConfig.java

--- src/main/java/io/github/adrianovictorn/syshelp/CorsConfig.java
+++ src/main/java/io/github/adrianovictorn/syshelp/CorsConfig.java
@@ -1,3 +1,5 @@
+package io.github.adrianovictorn.syshelp;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
